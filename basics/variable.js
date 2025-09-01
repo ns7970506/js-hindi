46cb8b18415995d1b837cc1c3a2032494134302c
Nikhil <118470357+ns7970506@users.noreply.github.com>
2025-09-01 06:13:45 +0000

    container config done

--- basics/variable.js
+++ basics/variable.js
@@ -2,4 +2,10 @@ const accountId =147
 let accountEmail = "nikhil1234@gmail.com"
 var accountpassword="12345"
 console.log(accountId);
-
+let accountstate;
+// accountId not allowed
+accountEmail="nikhil@gmail";
+accountpassword="18198";
+// we don't use var as a keyword because it a global 
+// variable so it can be changed and create confusing
+console.table([accountId,accountEmail,accountpassword,accountstate])
