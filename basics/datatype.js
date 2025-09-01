ebf8dd47806fbaafb3b1c83fd86f2fd47a8f988d
Nikhil <118470357+ns7970506@users.noreply.github.com>
2025-09-01 06:42:01 +0000

    datatype

---
+++ basics/datatype.js
@@ -0,0 +1,18 @@
+ "use strict";// treat all js code as newer version of javascript
+//alert("hello") we are using nodejs not browser
+let name="nikhil";//string data type
+let age=7// integer type
+let isloggedin=false // boolean type
+/*number 2 to the pow 53
+//bigint for large number of 
+// null =>stand alone value
+undefined = not defined
+symbol = unique
+
+
+
+object
+
+*/
+console.log(typeof undefined); // o/p undefined
+console.log(typeof null);// o/p object
