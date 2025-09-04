45e35954158af2692fa66a910a6c89bcfea49ee3
Nikhil <118470357+ns7970506@users.noreply.github.com>
2025-09-04 16:58:14 +0000

    add part1

---
+++ functions/part1.js
@@ -0,0 +1,17 @@
+function saymyname(){
+console.log("N");
+console.log("i");
+console.log("k");
+console.log("h");
+console.log("i");
+console.log("l");
+}
+// saymyname()
+
+// function addtwonumber(number1,number2){
+//     console.log(number1+number2);
+    
+// }
+
+const result = addtwonumber(4,8)
+console.log(result);
